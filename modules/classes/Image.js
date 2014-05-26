37ce8058a826ba67033517f918edbff5d2d25ba8
Kapil Easwar <bigpopakap@gmail.com>
2014-05-26 14:21:31 -0700

    added Image class for image resources

---
+++ modules/classes/Image.js
@@ -0,0 +1,47 @@
+/**
+ * Class to represent an image and associated data
+ * 
+ * @param url the path to the image
+ * @param options
+ * 		centerx - the location of the "center of focus" from left to right
+ * 					(can be undefined, default undefined) //TODO in pixels or %?
+ * 		centery - the location of the "center of focus" from top to bottom
+ * 					(can be undefined, default undefined) //TODO in pixels or %?
+ * 		alt - any caption or alternate text to go along with this Image
+ * 					(can be undefined, default undefined)
+ */
+function Image(url, options) {
+	
+	options = options || {};
+
+	function getUrl() {
+		return url;
+	}
+	this.getUrl = getUrl;
+	
+	function getCenterX() {
+		return options.centerx;
+	}
+	this.getCenterX = getCenterX;
+	
+	function getCenterY() {
+		return centery;
+	}
+	this.getCenterY = getCenterY;
+	
+	function getCenter() {
+		return {
+			x: getCenterX(),
+			y: getCenterY()
+		}
+	}
+	this.getCenter = getCenter;
+	
+	function getAlt() {
+		return options.alt;
+	}
+	this.getAlt = getAlt;
+	
+}
+
+module.exports = Image;
---
+++ public/k/k-imgpos.js
@@ -0,0 +1,13 @@
+/**
+	Client-side script to keep HTML elements positioned correctly
+	
+	Attributes to add to DOM elements:
+		data-k-imgpos="1" - activates on DOM load
+		data-k-imgpos-x="<value>" - defines the "center of focus" for the x axis
+		data-k-imgpos-y="<value>" - defines the "center of focus" for the y axis
+*/
+(function() {
+	
+	
+	
+})();
